18064821f2ffc62f4d4069b7e72f170743fae591
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2020-05-27 02:21:11 +0300

    redone

---
+++ NUnitTestProject3/NUnitTestProject3/LoginPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject3
+{
+    class LoginPage
+    {
+        private IWebDriver driver;
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        private IWebElement clickName => driver.FindElement(By.XPath("//input[@id='Name']"));
+        private IWebElement clickPussword => driver.FindElement(By.XPath("//input[@id='Password']"));
+
+        public void Login(string name, string pussword)
+        {
+            new Actions(driver).Click(clickName).SendKeys(name).Build().Perform();
+            new Actions(driver).Click(clickPussword).SendKeys(pussword).Build().Perform();
+            new Actions(driver).SendKeys(Keys.Enter).Build().Perform();
+        }
+    }
+}
---
+++ NUnitTestProject3/NUnitTestProject3/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace NUnitTestProject3
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private LoginPage loginPage;
+        private MainPage mainPage;
+        private LogoutPage logoutPage;
+
+
+        protected const string name = "user";
+        protected const string password = "user";
+
+        [OneTimeSetUp]
+        public void TestFixture()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [SetUp]
+        public void OneTimeSetUp()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+        }
+
+        [Test, Order(1)]
+        public void Login()
+        {
+            loginPage = new LoginPage(driver);
+            loginPage.Login(name, password);
+            Assert.AreEqual("Home page", driver.FindElement(By.XPath("//div[h2= 'Home page']")).Text);
+        }
+
+        [Test, Order(2)]
+        public void AddProduct()
+        {
+            mainPage = new MainPage(driver);
+            mainPage.AddProduct();
+            Thread.Sleep(2000);
+            Assert.AreEqual("Test", driver.FindElement(By.XPath("//a[contains(text(), 'Test')]")).Text);
+        }
+
+        [Test, Order(3)]
+        public void Logout()
+        {
+            logoutPage = new LogoutPage(driver);
+            logoutPage.Logout();
+            Assert.AreEqual("Login", driver.FindElement(By.XPath("//div[h2= 'Login']")).Text);
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
+
